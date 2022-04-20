d58bd60ce96f68bf54f81ce5ec68f832dd2748e3
Joaopcamposs <joaopcamposs@outlook.com>
2022-04-19 23:21:28 -0300

    Add docker-compose to project

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,16 +2,17 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql+pymysql://root:1234@localhost:3306/projetoWeb"
-#root é o usuário
-#1234 é a senha
-#3306 é a porta
-#projetoWeb é o schema
+DATABASE_URL = "mysql+pymysql://root:password@mysql_db/projetoWeb"
+# root é o usuário
+# password é a senha
+# mysql_db é o servico definido no docker-compose
+# projetoWeb é o database
 
 db_engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=db_engine)
 
 Base = declarative_base()
+
 
 def get_db():
     """
